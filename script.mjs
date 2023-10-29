1a408fdc3199782cd5b7881d643a84174f517beb
humirehman <humarehman429@gmail.com>
2023-10-29 03:20:40 -0700

    my changes

--- script.mjs
+++ script.mjs
@@ -1,12 +1,12 @@
-function convertCelsiusToFahrenheit() {
-    // Get the value entered by the user
+function convertCelsiusToFahrenheit(event) { 
+event.preventDefault();
     var celsius = parseFloat(document.getElementById('celsiusInput').value);
 
-    // Check if the input is a valid number
+   
     if (!isNaN(celsius)) {
-        // Convert Celsius to Fahrenheit
+       
         var fahrenheit = (celsius * 9/5) + 32;
-        // Display the result
+
         document.getElementById('result').innerHTML = celsius + " degrees Celsius is " + fahrenheit.toFixed(2) + " degrees Fahrenheit.";
     } else {
         // Display an error message if the input is not a valid number
